5f07b8b2d0ce08dcdc40e8e8bd95b451d83890b0
AwanCA <itsawanca@gmail.com>
2024-07-13 15:37:41 +0200

    feat: add functionality to removeFromArray and pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...dataToRemove) {
+    let outputArray = [];
+    for (let item of givenArray){
+        let toRemove = false;
+        for (let data of dataToRemove){
+            if (item === data){
+                toRemove = true;
+                break;
+            }
+        }
+        if (toRemove === true){
+            continue;
+        }
+        else{
+            outputArray.push(item);
+        }
+    }
+    return outputArray;
 };
 
 // Do not edit below this line
