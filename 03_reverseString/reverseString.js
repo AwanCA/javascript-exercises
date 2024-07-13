9331b69281f9ca1332fc66bafe1ae732c0fddada
AwanCA <itsawanca@gmail.com>
2024-07-13 14:00:13 +0200

    feat: add function reverseString and test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = [];
+    for (let i = 0; i < string.length; i++){
+        stringArray.unshift(string[i]);
+    }
+    let output = stringArray.join("")
+    return output;
 };
 
 // Do not edit below this line
