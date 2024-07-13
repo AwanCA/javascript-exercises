ac645a9aaa626b326d98a43d54881a61485bb60c
AwanCA <itsawanca@gmail.com>
2024-07-13 19:41:50 +0200

    feat: finish first part

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    if(!(Number.isInteger(startInt)) || !(Number.isInteger(endInt)) || startInt < 0 || endInt <0){
+        return "ERROR"
+    }
+    let output = 0
+    let lowerInt = 0
+    let higherInt = 0
 
+    if (startInt >= endInt){
+        lowerInt = endInt;
+        higherInt = startInt;
+    } else {
+        lowerInt = startInt;
+        higherInt = endInt;
+    }
+
+    for (let i = lowerInt; i<= higherInt; i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
